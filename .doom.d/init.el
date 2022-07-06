a785d0f7cbed62a5546cebc1cc8b864a549926bf
A-dot-S-dot <alexey.schwarzmann@tu-dortmund.de>
2022-07-06 14:56:04 +0200

    first commit: new start new luck

---
+++ .doom.d/init.el
@@ -0,0 +1,63 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;; Workaround for an error
+
+(doom! :completion
+       company
+       (vertico +icons)
+
+       :ui
+       doom
+       doom-dashboard
+       hl-todo
+       hydra
+       modeline
+       ophints
+       (popup +all +defaults)
+       vc-gutter
+       nav-flash
+       indent-guides
+       (treemacs +lsp)
+
+       :editor
+       (evil +everywhere)
+       file-templates
+       fold
+       (format +onsave)
+       snippets
+
+       :emacs
+       (dired +icons +ranger)
+       electric
+       undo
+       vc
+
+       :term
+       vterm
+
+       :email
+       (mu4e +org)
+
+       :checkers
+       syntax
+       (spell +flyspell +everywhere)
+
+       :tools
+       biblio
+       (debugger +lsp)
+       (eval +overlay)
+       (lookup +docsets +dictionary)
+       (lsp +peek)
+       magit
+       pdf
+
+       :lang
+       emacs-lisp
+       latex
+       (markdown +grip)
+       (org +pretty +roam2)
+       (python +lsp +pyright)
+
+       :config
+       literate
+       (default +bindings +smartparens))
---
+++ .doom.d/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+
+(package! visual-fill-column)
+(package! popup)
+(package! google-translate)
+
