a6f7dae29fd0b8125832d384facd2e5a0881dafa
A-dot-S-dot <alexey.schwarzmann@tu-dortmund.de>
2022-07-25 22:58:41 +0200

    Edited xmobar.

---
+++ .xmonad/lib/Colors/DoomOne.hs
@@ -0,0 +1,28 @@
+module Colors.DoomOne where
+
+import XMonad
+
+colorScheme = "doom-one"
+
+colorBack = "#282c34" --grey dark
+colorFore = "#bbc2cf" --grey light
+
+color01 = "#1c1f24"   --grey dark
+color02 = "#ff6c6b"   --red
+color03 = "#98be65"   --green
+color04 = "#da8548"   --orange
+color05 = "#51afef"   --blue
+color06 = "#c678dd"   --purple
+color07 = "#5699af"   --blue
+color08 = "#202328"   --grey dark
+color09 = "#5b6268"   --grey light
+color10 = "#da8548"   --orange
+color11 = "#4db5bd"   --blue
+color12 = "#ecbe7b"   --gold
+color13 = "#3071db"   --blue
+color14 = "#a9a1e1"   --purple
+color15 = "#46d9ff"   --cyan
+color16 = "#dfdfdf"   --silver
+
+colorTrayer :: String
+colorTrayer = "--tint 0x282c34"
