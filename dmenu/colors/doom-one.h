9692b357487a520d2482ac3cd487f221945d3711
A-dot-S-dot <alexey.schwarzmann@tu-dortmund.de>
2022-07-26 06:38:37 +0200

    Added dmenu.

---
+++ dmenu/colors/doom-one.h
@@ -0,0 +1,8 @@
+static const char *colors[SchemeLast][2] = {
+    /*     fg         bg       */
+    [SchemeNorm] = {"#cccccc", "#282c34"},
+    [SchemeSel] = {"#1c1f24", "#c678dd"},
+    [SchemeSelHighlight] = {"#98be65", "#c678dd"},
+    [SchemeNormHighlight] = {"#98be65", "#282c34"},
+    [SchemeOut] = {"#000000", "#51afef"},
+};
